bd2a5d0734af1c5cc4b826e4370972cd17d9221f
CristhianQL <cristhianql@gmail.com>
2017-12-01 17:22:02 -0600

    cambio en json package

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mongoose=require('mongoose');
 const Cakes=require('./models/cakes')
 
 const app=express()
-const port=process.env.port || 3001
+const port=process.env.PORT || 3001
 
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
@@ -14,8 +14,37 @@ app.use(bodyParser.json())
 app.get('/api/cakes',(req,res)=>{
     res.send(200,{cakes:[]})
 })
+app.get('/api/cakes/:id',(req,res)=>{
+     
+})
 
+app.post('/api/cakes',(req,res)=>{
+    console.log(req.body);
+    let cake=new Cakes()
+    cake.name=req.body.name
+    cake.picture=req.body.picture
+    cake.price=req.body.price
+    cake.category=req.body.category
+    cake.description=req.body.description
+    cake.save((err,cakeStore)=>{
+        if(err) res.status(500).send({message:`error al guardar el queque en la base de datos ${err}`})
+        res.status(200).send({cake:cakeStore})
+    })
+   
+})
 
-app.listen((process.env.PORT || 3001),()=>{
-    console.log('Servidor node.js iniciado');
+app.put('/api/cakes/:id',(req,res)=>{
+    
+    })
+
+app.delete('/api/cakes/:id',(req,res)=>{
+        
+        })
+mongoose.createConnection('mongodb://localhost:27017/cakesAppProject',(err,res)=>{
+    if(err) {console.log(`Ocurrio un error cuando intentaba conectarse a la base
+    de datos  ${err}.`);}
+    console.log('Se conecto a la base de datos')
+    app.listen(port,()=>{
+        console.log('Servidor node.js iniciado');
+    })
 })
