575bad6c99729e6cc3c5948c3ab878c38ddcd477
crisgql <cristhianql@gmail.com>
2017-12-11 00:58:46 -0600

    cors

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,23 @@ api.all('/', function(req, res, next) {
    const cors=require('cors');
    
    api.use(cors({origin:true,credentials: true}));
+   // Add headers
+   api.use(function (req, res, next) {
+   
+       // Website you wish to allow to connect
+       res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+   
+       // Request methods you wish to allow
+       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+   
+       // Request headers you wish to allow
+       res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+   
+       // Set to true if you need the website to include cookies in the requests sent
+       // to the API (e.g. in case you use sessions)
+       res.setHeader('Access-Control-Allow-Credentials', true);
+   
+       // Pass to next layer of middleware
+       next();
+   });
 module.exports=api;
