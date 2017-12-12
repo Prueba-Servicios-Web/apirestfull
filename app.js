bde9b966ebed6e31d4aea700f9784c75638c0b29
crisgql <cristhianql@gmail.com>
2017-12-11 21:31:14 -0600

    ui deploy

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const express=require('express');
 
 const app=express()
 const api=require('./routes')
-
+var path = require('path');
+app.use("/content", express.static(__dirname + '/uitest/content'));
+app.use("/fonts", express.static(__dirname + '/uitest/fonts'));
 app.use('/api',api)
+
+app.get('/index', function (req, res) {
+    res.sendFile(path.join(__dirname + '/uitest/ui.html'));
+  });
 
 module.exports=app;
